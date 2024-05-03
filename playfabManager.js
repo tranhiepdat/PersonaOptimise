91e1442fbea4d163039f5293e4a5fc6b9f7e8f4a
tranhiepdat <tranhiepdatanimator@gmail.com>
2024-05-03 20:13:46 +0700

    First Commit

---
+++ playfabManager.js
@@ -0,0 +1,48 @@
+function Login() {
+    var uuid = new DeviceUUID().get();
+    PlayFab.settings.deviceId = uuid
+    PlayFab.settings.titleId = "B2C32"
+    var request = {
+        CustomId: PlayFab.settings.deviceId,
+        CreateAccount: true,
+        InfoRequestParameters: {
+            GetPlayerProfile: true
+        },
+        TitleId: PlayFab.settings.titleIditleId // Make sure to replace "YOUR_TITLE_ID" with your actual PlayFab title ID
+    };
+    
+    PlayFabClientSDK.LoginWithCustomID(request, LoginCallback);
+}
+
+var LoginCallback = function (result, error) {
+    if (result !== null) {
+        console.log("login success");
+        if (result.data.InfoResultPayload.PlayerProfile.DisplayName !== undefined) {
+            console.log(result.data.InfoResultPayload.PlayerProfile.DisplayName);
+            playerName = result.data.InfoResultPayload.PlayerProfile.DisplayName;
+            playerNameInput = true;
+        }
+    } else if (error !== null) {
+        console.log(PlayFab.GenerateErrorReport(error));
+    }
+}
+
+function UpdateDisplayName(){
+    var request = {
+        DisplayName: playerName
+    };
+    
+    PlayFabClientSDK.UpdateUserTitleDisplayName(request, UpdateDisplayNameCallback);
+}
+
+var UpdateDisplayNameCallback = function (result, error){
+    if (result !== null) {
+        console.log("name update success");
+        console.log(result.data.DisplayName);
+        playerName = result.data.DisplayName;
+        playerNameInput = true;
+    } else if (error !== null) {
+        console.log(PlayFab.GenerateErrorReport(error));
+    }
+}
+Login();
